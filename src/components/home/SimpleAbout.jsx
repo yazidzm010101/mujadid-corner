fd1b5a49c3913a0047f8066e152a887bfdc4b8e5
Yazid Zaidan Mujadid <yazidzm.developer@gmail.com>
2023-11-04 00:40:57 +0700

    fix 404 about

--- src/components/home/SimpleAbout.jsx
+++ src/components/home/SimpleAbout.jsx
@@ -132,7 +132,7 @@ function SimpleAbout() {
               data-aos={"scale-fade-left"}
               ml={"auto"}
               as={Link}
-              href={getFullUrl("/about")}
+              href={"/about"}
               iconSpacing={4}
               rightIcon={<Icon h={7} w={7} as={BsArrowRightCircleFill} />}
               rounded={"full"}
