81a6a46e76a1697452eb2baa0b18cb52eef775e6
Yazid Zaidan Mujadid <yazidzm.developer@gmail.com>
2023-11-03 13:32:56 +0700

    Fix favicon on gh pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,36 @@
 import "@/styles/globals.css";
-import "aos/dist/aos.css";
 import "animate.css";
+import "aos/dist/aos.css";
 
 import AOS from "aos";
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 import theme from "@/styles/theme";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  const getFullUrl = (url) => {
+    if (url.match(/^\//g)) {
+      return router.basePath + url;
+    }
+    return url;
+  };
+
   useEffect(() => {
     AOS.init();
     window.scrollTo(0, 0);
   }, []);
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <link rel="icon" href={getFullUrl("/favicon.ico")} sizes="any" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
